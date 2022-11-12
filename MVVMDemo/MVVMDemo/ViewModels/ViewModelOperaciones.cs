f680d56c84a4701264654b72755706e9768e5a27
Noe <noe@gmail.com>
2022-11-12 15:11:14 -0500

    ultimo commmit

--- MVVMDemo/MVVMDemo/ViewModels/ViewModelOperaciones.cs
+++ MVVMDemo/MVVMDemo/ViewModels/ViewModelOperaciones.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -10,6 +11,20 @@ namespace MVVMDemo.ViewModels
     {
 
         #region  Atributos
+        String message;
+        public string Message
+        {
+            get { return message; }
+            set
+            {
+                if (message != value)
+                {
+                    message = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         int valorA;
         public int ValorA
         {
@@ -52,9 +67,69 @@ namespace MVVMDemo.ViewModels
             }
         }
 
+        int resultadoResta;
+        public int ResultadoResta
+        {
+            get { return resultadoResta; }
+            set
+            {
+                if (resultadoResta != value)
+                {
+                    resultadoResta = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoDivicion;
+        public int ResultadoDivicion
+        {
+            get { return resultadoDivicion; }
+            set
+            {
+                if (resultadoDivicion != value)
+                {
+                    resultadoDivicion = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoMultiplicacion;
+        public int ResultadoMultiplicacion
+        {
+            get { return resultadoMultiplicacion; }
+            set
+            {
+                if (resultadoMultiplicacion != value)
+                {
+                    resultadoMultiplicacion = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoDivision;
+        public int ResultadoDivision
+        {
+            get { return resultadoDivision; }
+            set
+            {
+                if (resultadoDivision != value)
+                {
+                    resultadoDivision = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
         #endregion
 
         public ICommand Sumar { protected set; get; }
+        public ICommand Restar { protected set; get; }
+        public ICommand Multiplicar { protected set; get; }
+        public ICommand Dividir { protected set; get; }
+
 
 
         public ViewModelOperaciones()
@@ -62,6 +137,28 @@ namespace MVVMDemo.ViewModels
             Sumar = new Command(() =>
             {
                 ResultadoSuma = ValorA + ValorB;
+            });
+
+            Restar = new Command(() =>
+            {
+                ResultadoResta = ValorA - ValorB;
+            });
+
+            Multiplicar = new Command(() =>
+            {
+                ResultadoMultiplicacion = ValorA * ValorB;
+            });
+
+            Dividir = new Command(() =>
+            {
+                if (ValorB == 0)
+                {
+                    Message = "No puedes dividir entre cero";
+                }
+                else
+                {
+                    ResultadoDivision = ValorA / ValorB;
+                }
             });
 
         }       
